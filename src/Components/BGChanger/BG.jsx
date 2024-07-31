87520fa1b44abb049e05afb08a44e66343dc4134
manish devlikar <manish.devlikar5003@gmail.com>
2024-07-31 12:52:40 +0530

    bg-changer code updated

--- src/Components/BGChanger/BG.jsx
+++ src/Components/BGChanger/BG.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import { useState } from "react"
 
 const BG = () => {
+
+  const [bgColor, setBgColor] = useState("white");
+
   return (
-    <div>
-      
+    <div className='w-full h-screen flex justify-center' style={{backgroundColor:bgColor}}>
+      <div className="h-12 w-9/12 bg-transparent flex justify-center align-middle gap-5 rounded-full">
+         <button className=" w-20 px-3 py-1 rounded-full bg-black text-white outline-none ring-1 ring-red-600" onClick={()=>setBgColor("black")}>black</button>
+         <button className=" w-20 px-3 py-1 rounded-full bg-white text-black outline-none ring-1 ring-red-600" onClick={()=>setBgColor("white")}>white</button>
+         <button className=" w-20 px-3 py-1 rounded-full bg-pink-500 text-black outline-none ring-1 ring-red-600" onClick={()=>setBgColor("pink")}>pink</button>
+         <button className=" w-20 px-3 py-1 rounded-full bg-green-600 text-black outline-none ring-1 ring-red-600" onClick={()=>setBgColor("green")}>green</button>
+         <button className=" w-20 px-3 py-1 rounded-full bg-orange-500 text-white outline-none ring-1 ring-red-600" onClick={()=>setBgColor("orange")}>orange</button>
+      </div>
     </div>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,12 @@ import ReactDOM from 'react-dom/client'
 import './css/index.css'
 import Form from './Components/Form.jsx'
 import Counter from './Components/Counter/Counter.jsx'
+import BG from './Components/BGChanger/BG.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     {/* <Form /> */}
-    <Counter/>
+    {/* <Counter/> */}
+    <BG/>
   </React.StrictMode>,
 )
