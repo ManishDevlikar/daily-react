041c1ed38169534ab7f3c4133a94bfd3accbb75c
manish devlikar <manish.devlikar5003@gmail.com>
2024-08-24 20:14:18 +0530

    add use hook

--- src/Components/CustomHook/Home.jsx
+++ src/Components/CustomHook/Home.jsx
@@ -1,7 +1,10 @@
+// import { LoggedContext } from './LoggedContext'
 import { useIsLogged } from './useIsLogged'
 
 function Home() {
     const {isLogged,setlogged} = useIsLogged(false);
+    // const {isLogged,setLogged} = use(LoggedContext);  // react 19 feature
+
   return (
     <div>
       <h1>user is {isLogged?"active":"inactive"}</h1>
